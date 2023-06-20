057e19cfba3cbc3eb6be3c4fa549d21a579261f8
Felipe Mello <felipesilva.mello27@gmail.com>
2023-06-20 20:21:03 -0300

    Processamento de dados em Java, Casting

---
+++ Seção 4 - Estrutura sequencial/processamento_de_dados_em_Java_Casting/src/processamento_de_dados_em_Java_Casting/Main.java
@@ -0,0 +1,48 @@
+package processamento_de_dados_em_Java_Casting;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		int x;
+//		double y;
+//		
+//		x = 5;
+//		y = 2 * x;
+//		
+//		System.out.println(x);
+//		System.out.println(y);
+//		System.out.println();
+		
+//		double b, B, h, area;
+//		
+//		b = 6.0;
+//		B = 8.0;
+//		h = 5.0;
+//		
+//		area = (B + b) / 2 * h;
+//		
+//		System.out.println(area);
+//		System.out.println();
+		
+//		int a , b;
+//		double resultado; 
+//		
+//		a = 5;
+//		b = 2;
+//		resultado = (double) a / b;
+//		
+//		System.out.println(resultado);
+//		System.out.println();
+		
+		double a;
+		int b;
+		
+		a = 5.0;
+		b = (int) a;
+		
+		System.out.println(b);
+		System.out.println();
+
+	}
+
+}
