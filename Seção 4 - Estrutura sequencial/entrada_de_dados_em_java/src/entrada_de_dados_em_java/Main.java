181a4fca1a913913eafb5b06e35aa62e357b96aa
Felipe Mello <felipesilva.mello27@gmail.com>
2023-06-20 20:58:17 -0300

    Entrada de dados em Java - Parte 2

--- Seção 4 - Estrutura sequencial/entrada_de_dados_em_java/src/entrada_de_dados_em_java/Main.java
+++ Seção 4 - Estrutura sequencial/entrada_de_dados_em_java/src/entrada_de_dados_em_java/Main.java
@@ -8,36 +8,49 @@ public class Main {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 //		String x;
 //		x = sc.next();
-		
+
 //		int x;
 //		x = sc.nextInt();
-		
+
 //		double x;
 //		x = sc.nextDouble();
-		
+
 //		System.out.printf("Você digitou: %.2f%n", x);
-		
+
 //		char x;
 //		x = sc.next().charAt(0);
-		
-		String x;
-		int y;
-		double z;
-		
-		x = sc.next();
-		y = sc.nextInt();
-		z = sc.nextDouble();
-		
+
+//		String x;
+//		int y;
+//		double z;
+//		
+//		x = sc.next();
+//		y = sc.nextInt();
+//		z = sc.nextDouble();
+//		
+//		System.out.println("Dados digitados:");
+//		System.out.println(x);
+//		System.out.println(y);
+//		System.out.println(z);
+//		
+//		System.out.println("Você digitou: " + x);
+
+		String s1, s2, s3;
+
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
+
 		System.out.println("Dados digitados:");
-		System.out.println(x);
-		System.out.println(y);
-		System.out.println(z);
-		
-		System.out.println("Você digitou: " + x);
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+
 		sc.close();
+
 	}
 
 }
