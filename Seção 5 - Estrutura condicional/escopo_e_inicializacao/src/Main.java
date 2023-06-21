05f417e67e34ce1595fc78ad08abbdbf0ec3e53a
Felipe Mello <felipesilva.mello27@gmail.com>
2023-06-20 23:10:36 -0300

    Escopo e inicialização

---
+++ Seção 5 - Estrutura condicional/escopo_e_inicializacao/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		double price = 400.0;
+
+		if (price < 200) {
+			double discount = price * 0.1;
+		}
+
+//		System.out.println(discount); // error
+
+	}
+
+}
